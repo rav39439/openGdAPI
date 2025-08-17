1ba2913f46bdcace981b3d68e908b5cb03221669
rav39439 <ravish.kumar@ispf.ngo>
2025-08-17 13:21:29 +0530

    change in db

--- src/db/db.js
+++ src/db/db.js
@@ -1,20 +1,43 @@
-// redisClient.js
+// // redisClient.js
 const { createClient } = require('redis');
 
 
+// // const client = createClient({
+// //   url: redisUrl
+// // });
+
 // const client = createClient({
-//   url: redisUrl
+//   // url: `redis://${process.env.REDIS_HOST || "localhost"}:${process.env.REDIS_PORT || 6379}`,
+//     url: `rediss://default:${process.env.REDIS_TOKEN}@exotic-raccoon-51584.upstash.io:6379`
+
 // });
 
-const client = createClient({
-  // url: `redis://${process.env.REDIS_HOST || "localhost"}:${process.env.REDIS_PORT || 6379}`,
-    url: `rediss://default:${process.env.REDIS_TOKEN}@exotic-raccoon-51584.upstash.io:6379`
 
+// client.on('error', (err) => console.error('❌ Redis Error:', err));
+// client.on('connect', () => console.log('✅ Connected to Redis'));
+
+// (async () => {
+//   if (!client.isOpen) {
+//     await client.connect();
+//   }
+// })();
+
+// module.exports = client;
+
+
+const client = createClient({
+  url: `rediss://default:${process.env.REDIS_TOKEN}@exotic-raccoon-51584.upstash.io:6379`,
+  socket: {
+    reconnectStrategy: (retries) => {
+      console.log(`Reconnecting attempt #${retries}`);
+      return Math.min(retries * 50, 5000); // retry delay in ms
+    }
+  }
 });
 
-
-client.on('error', (err) => console.error('❌ Redis Error:', err));
+client.on('error', (err) => console.error('Redis Error:', err));
 client.on('connect', () => console.log('✅ Connected to Redis'));
+client.on('ready', () => console.log('🔵 Redis ready'));
 
 (async () => {
   if (!client.isOpen) {
